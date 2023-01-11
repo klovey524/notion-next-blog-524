5031b9f7ab43ce5f0f9f69f8ddaa4008f46f385e
klovey524 <konikasu0305@gmail.com>
2023-01-11 20:45:08 +0900

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage<IndexProps> = ({ pages }) => {
   return (
     <Layout>
       <div className="pt-12">
-        <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
+        <h1 className="text-5xl mb-8">ALL POST</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {/* Card */}
           {pages.map((page, index) => (
