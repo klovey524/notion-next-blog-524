90792c618515a78189ab08969acd5cc1e6833500
klovey524 <konikasu0305@gmail.com>
2023-01-11 21:52:31 +0900

    Update

--- site.config.ts
+++ site.config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: "NotionBlog-524-",
   twitterUrl: "", // ご自身の値に書き換えてください
   instagramUrl: "", // ご自身の値に書き換えてください
-  githubUrl: "", // ご自身の値に書き換えてください
+  githubUrl: "https://github.com/klovey524", // ご自身の値に書き換えてください
 };
